20359c53713a8a93e98c60c0f39bed9e0c7dd02a
Thomas Coady <100371507+coadythecoder@users.noreply.github.com>
2026-04-11 10:58:59 -0400

    ALU first draft

--- p4/alu.vhd
+++ p4/alu.vhd
@@ -20,12 +20,15 @@ begin
         variable funct7 : std_logic_vector(6 downto 0);
         variable rs1 : signed(31 downto 0);
         variable rs2 : signed(31 downto 0);
+        variable temp : signed(63 downto 0);
+        variable shift_amount : unsigned(4 downto 0);
     begin
         rs1 := signed(A);
         rs2 := signed(B);
         opcode := instruction(6 downto 0);
         funct3 := instruction(14 downto 12);
         funct7 := instruction(31 downto 25);
+        shift_amount := unsigned(B)(4 downto 0);
         case opcode is 
             when '0110011' =>
                 case funct3 is
@@ -33,7 +36,8 @@ begin
                         if funct7 = x"00" then -- add
                             result <= std_logic_vector(rs1 + rs2);
                         elsif funct7 = x"01" then -- mul
-                            -- mul
+                            temp := rs1 * rs2;
+                            result <= std_logic_vector(temp(31 downto 0));
                         elsif funct7 = x"20" then -- sub
                             result <= std_logic_vector(rs1 - rs2);
                         end if;
@@ -44,20 +48,28 @@ begin
                     when x"1" => -- sll
                         result <= rs1 << rs2
                     when x"5" =>
-                        -- shift right logical or shift right arithmetic
+                        if funct7 = x"00" then -- shift right logical
+                            result <= std_logic_vector(shift_right(unsigned(A), shift_amount));
+                        elsif funct7 = x"20" then -- shift right arithmetic
+                            result <= std_logic_vector(shift_right(rs1, shift_amount));
+                        end if;
                 end case;
             when '0010011' =>
                 case funct3 is
-                    when x"0" =>
-                        -- add immediate
-                    when x"4" =>
-                        -- xor immediate
-                    when x"6" => 
-                        -- or immediate
-                    when x"7" =>
-                        -- and immediate
-                    when x"2" =>
-                        -- set less than immediate
+                    when x"0" => -- add imm
+                        result <= std_logic_vector(rs1 + rs2);
+                    when x"4" => -- xor imm
+                        result <= A xor B;
+                    when x"6" => -- or imm
+                        result <= A or B;
+                    when x"7" => -- and imm
+                        result <= A and B;
+                    when x"2" => -- set less than immediate (rd = (rs1<imm)?1:0)
+                        if rs1 < rs2 then
+                            result <= to_std_logic_vector(1, 32);
+                        else
+                            result <= to_std_logic_vector(0, 32);
+                        end if;
                 end case;
         end case;
     end process;
