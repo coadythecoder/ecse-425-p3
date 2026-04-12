ca3d44d1d101831d6081ea4375a457c9a433e7ac
Thomas Coady <100371507+coadythecoder@users.noreply.github.com>
2026-04-11 22:08:43 -0400

    Fix compilation errors

--- p4/alu.vhd
+++ p4/alu.vhd
@@ -16,21 +16,23 @@ begin
     -- stuff
     process (instruction)
         variable opcode : std_logic_vector(6 downto 0);
-        variable funct3 : std_logic_vector(2 downto 0);
-        variable funct7 : std_logic_vector(6 downto 0);
+        variable funct3 : std_logic_vector(3 downto 0);
+        variable funct7 : std_logic_vector(7 downto 0);
         variable rs1 : signed(31 downto 0);
         variable rs2 : signed(31 downto 0);
         variable temp : signed(63 downto 0);
-        variable shift_amount : unsigned(4 downto 0);
+        variable shift_amount : integer;
+        variable op2_unsigned : unsigned(31 downto 0);
     begin
         rs1 := signed(op1);
         rs2 := signed(op2);
         opcode := instruction(6 downto 0);
-        funct3 := instruction(14 downto 12);
-        funct7 := instruction(31 downto 25);
-        shift_amount := unsigned(op2)(4 downto 0);
+        funct3 := "0" & instruction(14 downto 12);
+        funct7 := "0" & instruction(31 downto 25);
+        op2_unsigned := unsigned(op2);
+        shift_amount := to_integer(op2_unsigned(4 downto 0));
         case opcode is 
-            when '0110011' =>
+            when "0110011" =>
                 case funct3 is
                     when x"0" =>
                         if funct7 = x"00" then -- add
@@ -46,15 +48,17 @@ begin
                     when x"7" => -- and
                         result <= op1 and op2;
                     when x"1" => -- sll
-                        result <= std_logic_vector(shift_left(rs1, shift_ammount));
+                        result <= std_logic_vector(shift_left(rs1, shift_amount));
                     when x"5" =>
                         if funct7 = x"00" then -- shift right logical
                             result <= std_logic_vector(shift_right(unsigned(op1), shift_amount));
                         elsif funct7 = x"20" then -- shift right arithmetic
                             result <= std_logic_vector(shift_right(rs1, shift_amount));
                         end if;
+                    when others =>
+                        result <= (others => '1'); -- all 1's to be very obvious when this case happens
                 end case;
-            when '0010011' =>
+            when "0010011" =>
                 case funct3 is
                     when x"0" => -- add imm
                         result <= std_logic_vector(rs1 + rs2);
@@ -66,25 +70,29 @@ begin
                         result <= op1 and op2;
                     when x"2" => -- set less than immediate (rd = (rs1<imm)?1:0)
                         if rs1 < rs2 then
-                            result <= to_std_logic_vector(1, 32);
+                            result <= std_logic_vector(to_unsigned(1, 32));
                         else
-                            result <= to_std_logic_vector(0, 32);
+                            result <= std_logic_vector(to_unsigned(0, 32));
                         end if;
+                    when others =>
+                        result <= (others => '1'); -- all 1's to be very obvious when this case happens
                 end case;
-            when '0000011' => -- load word
+            when "0000011" => -- load word
                 result <= std_logic_vector(rs1 + rs2); -- rs1 + imm for addressing memory
-            when '0100011' => -- store word
+            when "0100011" => -- store word
                 result <= std_logic_vector(rs1 + rs2); -- rs1 + imm for addressing memory
-            when '1100011' => -- branch
+            when "1100011" => -- branch
                 result <= std_logic_vector(rs1 + rs2); -- do i need to shift imm (rs2)??? how much???
-            when '1101111' => -- jump and link
+            when "1101111" => -- jump and link
                 result <= std_logic_vector(rs1 + rs2); -- rs1=PC, rs2=imm do i need to shift imm??? how much???
-            when '1100111' => -- jump and link reg
+            when "1100111" => -- jump and link reg
                 result <= std_logic_vector(rs1 + rs2); -- rs1=rs1, rs2=imm do i need to shift imm??? how much??
-            when '0110111' => -- load upper imm
+            when "0110111" => -- load upper imm
                 result <= std_logic_vector(shift_left(rs1, 12));
-            when '0010111' => -- add upper imm to pc
+            when "0010111" => -- add upper imm to pc
                 result <= std_logic_vector(rs1 + shift_left(rs1, 12));
+            when others =>
+                result <= (others => '1');  -- all 1's to be very evident when this case happens
         end case;
     end process;
 end arch;
