55add58508de6e8433758a745e8af7c0cbba98ba
Thomas Coady <100371507+coadythecoder@users.noreply.github.com>
2026-04-11 21:09:38 -0400

    Add control logic in DECODE

--- p4/processor.vhd
+++ p4/processor.vhd
@@ -71,7 +71,7 @@ architecture arch of processor is
     signal mux_a_select : std_logic; -- selector for mux_a
     signal mux_b_select : std_logic;  -- selector for mux_b
     signal mux_pc_select : std_logic; -- selector for mux_pc
-    signal mux_write_select : std_logic; -- selector for mux_write
+    signal mux_write_select : integer range 0 to 2; -- selector for mux_write
 
     type state_type is (FETCH, DECODE, EXECUTE, MEMORY, WRITEBACK);
     signal state : state_type;
@@ -81,8 +81,8 @@ begin
         clock => clock,
         writedata => B,
         address => alu_out,
-        memwrite => open,
-        memread => open,
+        memwrite => mem_write,
+        memread => mem_read,
         readdata => lmd,
         waitrequest => open
     );
@@ -104,6 +104,7 @@ begin
         read_addr2 => ir(24 downto 20),
         write_addr => ir(11 downto 7),
         write_data => mux_write,
+        write_enable => reg_write,
         read_data1 => A,
         read_data2 => B
     );
@@ -115,8 +116,10 @@ begin
         result => alu_out;
     );
 
-    mux_a <= A when mux_a_select='0' else npc;
-    mux_b <= B when mux_b_select='1' else imm;
+    mux_a <= A when mux_a_select = '0' else npc;
+    mux_b <= B when mux_b_select = '1' else imm;
+    mux_pc <= alu_out when mux_pc_select = '0' else std_logic_vector(to_unsigned(npc, 32));
+    mux_write <= alu_out when mux_write_select = 0 else lmd when mux_write_select = 1 else std_logic_vector(to_unsigned(npc, 32));
 
     cpu_process: process(clock, reset)
         variable opcode : std_logic_vector(6 downto 0);
@@ -137,23 +140,43 @@ begin
                     imm_raw := (others => '0');
                     
                     case opcode is
+                        when "0110011" => -- R-type
+                            -- no immediate value to deal with in R-type
+                        
+                            mux_a_select <= '0';
+                            mux_b_select <= '0';
                         when "0010011" | "0000011" | "1100111"=> -- I-type
                             imm_raw(11 downto 0) := ir(31 downto 20);
+                            
+                            mux_a_select <= '0'; -- op1 := A
+                            mux_b_select <= '1'; -- op2 := B
                         when "0100011" => -- S-type
                             imm_raw(11 downto 5) := ir(31 downto 25);
                             imm_raw(4 downto 0) := ir(11 downto 7);
+
+                            mux_a_select <= '0'; -- op1 := A
+                            mux_b_select <= '0'; -- op2 := imm
                         when "1100011" => -- B-type
                             imm_raw(12) := ir(31);
                             imm_raw(10 downto 5) := ir(30 downto 25);
                             imm_raw(4 downto 1) := ir(11 downto 8);
                             imm_raw(11) := ir(7);
+
+                            mux_a_select <= '1'; -- op1 := PC
+                            mux_b_select <= '0'; -- op2 := imm
                         when "0110111" | "0010111" => -- U-type
                             imm_raw(31 downto 12) := ir(31 downto 12);
+
+                            mux_a_select <= '0';
+                            mux_b_select <= '0';
                         when "1101111" => -- J-type
                             imm_raw(20) := ir(31);
                             imm_raw(10 downto 1) := ir(30 downto 21);
                             imm_raw(11) := ir(20);
                             imm_raw(19 downto 12) := ir(19 downto 12);
+
+                            mux_a_select <= '0';
+                            mux_b_select <= '0';
                     end case;
 
                     imm <= imm_raw;
